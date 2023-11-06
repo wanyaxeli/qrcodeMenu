1c272d6adbd4859f2a887431691212303d6c546b
Wanyaxeli <eliwanyax@gmail.com>
2023-11-06 21:43:50 +0300

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import "slick-carousel/slick/slick-theme.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <Router basename='/'>
+   <Router basename='/qrcodeMenu'>
    <App />
    </Router>
   </React.StrictMode>
